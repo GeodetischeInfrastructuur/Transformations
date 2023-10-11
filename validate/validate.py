aae2a3be8bc025df63f533c70f82e980e3a4888b
Wouter <wouter.visscher@gmail.com>
2023-10-11 11:15:52 +0200

    switched to TransformerGroup so Transformation info is echoed by pyproj

--- validate/validate.py
+++ validate/validate.py
@@ -1,10 +1,10 @@
-from pyproj import Transformer, datadir
+from pyproj import transformer, datadir
 import sys
 
 print("PATH of the used PROJ: " + datadir.get_data_dir())
 
-rdnap2etrf = Transformer.from_crs("EPSG:7415", "EPSG:7931")
-etrf2rdnap = Transformer.from_crs("EPSG:7931", "EPSG:7415")
+rdnap2etrf = transformer.TransformerGroup("EPSG:7415", "EPSG:7931")
+etrf2rdnap = transformer.TransformerGroup("EPSG:7931", "EPSG:7415")
 
 result = (
     ",".join(
@@ -41,8 +41,8 @@ with open(sys.argv[1], "r") as Z001:
         if nap == "*":
             nap = float("inf")
 
-        lat_calc, lon_calc, h_calc = rdnap2etrf.transform(rdx, rdy, nap)
-        rdx_calc, rdy_calc, nap_calc = etrf2rdnap.transform(lat, lon, h)
+        lat_calc, lon_calc, h_calc = rdnap2etrf.transformers[0].transform(rdx, rdy, nap)
+        rdx_calc, rdy_calc, nap_calc = etrf2rdnap.transformers[0].transform(lat, lon, h)
 
         lat_diff, lon_diff, h_diff = (
             float(lat) - lat_calc,
