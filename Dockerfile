c561df75fff18b2d57bf1df77534574c2cb768bf
lhuisman <l.huisman@geopinie.nl>
2024-01-17 13:54:50 +0100

    added bes 2d

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_00_authorities.sql | sqlite3 /usr/sh
 RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_10_copy_transformations_from_projdb.sql | sqlite3 /usr/share/proj/proj.db
 RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_20_datum_and_crs.sql | sqlite3 /usr/share/proj/proj.db
 RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_30_nl_transformations.sql | sqlite3 /usr/share/proj/proj.db
-RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_40_europe_transformations.sql | sqlite3 /usr/share/proj/proj.db
+RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_40_regional_transformations.sql | sqlite3 /usr/share/proj/proj.db
 RUN cat /usr/share/proj/nl_nsgi_sql/nl_nsgi_50_wgs84_null_transformations.sql | sqlite3 /usr/share/proj/proj.db
 
 RUN cp /usr/share/proj/proj.db /usr/share/proj/proj.global.time.dependent.transformations.db
--- sql/nl_nsgi_20_datum_and_crs.sql
+++ sql/nl_nsgi_20_datum_and_crs.sql
@@ -87,6 +87,63 @@ VALUES
         '1027'
     );
 
+INSERT INTO
+    "geodetic_datum" (
+        auth_name,
+        code,
+        name,
+        description,
+        ellipsoid_auth_name,
+        ellipsoid_code,
+        prime_meridian_auth_name,
+        prime_meridian_code,
+        publication_date,
+        frame_reference_epoch,
+        ensemble_accuracy,
+        anchor,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet',
+        'Geodetic Datum for BES',
+        NULL,
+        'EPSG',
+        '7022',
+        'EPSG',
+        '8901',
+        '2023-12-11',
+        NULL,
+        NULL,
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_USAGE',
+        'geodetic_datum',
+        'NSGI',
+        'DPnet',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
 -------------------------------------------------------
 --     Add additonal NL crss 
 --     (pseudo RD Bessel, AGRS2010)
@@ -307,3 +364,512 @@ VALUES
         '1027'
     );
 
+--- Bonaire 
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Bonaire_GEOCENTRIC',
+        'DPnet Bonaire',
+        NULL,
+        'geocentric',
+        'EPSG',
+        '6500',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Bonaire__GEOCENTRIC_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_Bonaire_GEOCENTRIC',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Bonaire_GEOGRAPHIC_3D',
+        'DPnet Bonaire',
+        NULL,
+        'geographic 3D',
+        'EPSG',
+        '6423',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Bonaire_GEOGRAPHIC_3D_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_Bonaire_GEOGRAPHIC_3D',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Bonaire_GEOGRAPHIC_2D',
+        'DPnet Bonaire',
+        NULL,
+        'geographic 2D',
+        'EPSG',
+        '6422',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Bonaire_GEOGRAPHIC_2D_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_Bonaire_GEOGRAPHIC_2D',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+    
+--- Saba
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Saba_GEOCENTRIC',
+        'DPnet Saba',
+        NULL,
+        'geocentric',
+        'EPSG',
+        '6500',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Saba__GEOCENTRIC_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_Saba_GEOCENTRIC',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Saba_GEOGRAPHIC_3D',
+        'DPnet Saba',
+        NULL,
+        'geographic 3D',
+        'EPSG',
+        '6423',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Saba_GEOGRAPHIC_3D_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_Saba_GEOGRAPHIC_3D',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Saba_GEOGRAPHIC_2D',
+        'DPnet Saba',
+        NULL,
+        'geographic 2D',
+        'EPSG',
+        '6422',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_Saba_GEOGRAPHIC_2D_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_Saba_GEOGRAPHIC_2D',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+
+--- St. Eustatius
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_St_Eustatius_GEOCENTRIC',
+        'DPnet St_Eustatius',
+        NULL,
+        'geocentric',
+        'EPSG',
+        '6500',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_St_Eustatius_GEOCENTRIC_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_St_Eustatius_GEOCENTRIC',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_St_Eustatius_GEOGRAPHIC_3D',
+        'DPnet St_Eustatius',
+        NULL,
+        'geographic 3D',
+        'EPSG',
+        '6423',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_St_Eustatius_GEOGRAPHIC_3D_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_St_Eustatius_GEOGRAPHIC_3D',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+INSERT INTO
+    "geodetic_crs" (
+        auth_name,
+        code,
+        name,
+        description,
+        type,
+        coordinate_system_auth_name,
+        coordinate_system_code,
+        datum_auth_name,
+        datum_code,
+        text_definition,
+        deprecated
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_St_Eustatius_GEOGRAPHIC_2D',
+        'DPnet St_Eustatius',
+        NULL,
+        'geographic 2D',
+        'EPSG',
+        '6422',
+        'NSGI',
+        'AGRS.NL',
+        NULL,
+        0
+    );
+
+INSERT INTO
+    "usage" (
+        auth_name,
+        code,
+        object_table_name,
+        object_auth_name,
+        object_code,
+        extent_auth_name,
+        extent_code,
+        scope_auth_name,
+        scope_code
+    )
+VALUES
+    (
+        'NSGI',
+        'DPnet_St_Eustatius_GEOGRAPHIC_2D_USAGE',
+        'geodetic_crs',
+        'NSGI',
+        'DPnet_St_Eustatius_GEOGRAPHIC_2D',
+        'EPSG',
+        '3805',
+        'EPSG',
+        '1027'
+    );
+
+--- Map projections
+INSERT INTO "conversion" VALUES('NSGI','DPnet_Bonaire_conversion','Local map projection system of Bonaire','','EPSG','9807','Transverse Mercator','EPSG','8801','Latitude of natural origin',12.180658675,'EPSG','9102','EPSG','8802','Longitude of natural origin',-68.251802281,'EPSG','9102','EPSG','8805','Scale factor at natural origin',1.0,'EPSG','9201','EPSG','8806','False easting',23209.5600,'EPSG','9001','EPSG','8807','False northing',21423.9900,'EPSG','9001',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+INSERT INTO "usage" VALUES('NSGI','DPnet_Bonaire_conversion_USAGE','conversion','NSGI','DPnet_Bonaire_conversion','EPSG','3805','EPSG','1027');
+
+INSERT INTO "conversion" VALUES('NSGI','DPnet_Saba_conversion','Local map projection of Saba','','EPSG','9807','Transverse Mercator','EPSG','8801','Latitude of natural origin',0.0,'EPSG','9102','EPSG','8802','Longitude of natural origin',-63.0,'EPSG','9102','EPSG','8805','Scale factor at natural origin',0.9996,'EPSG','9201','EPSG','8806','False easting',29973.97,'EPSG','9001','EPSG','8807','False northing',1947925.94,'EPSG','9001',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+INSERT INTO "usage" VALUES('NSGI','DPnet_Saba_conversion_USAGE','conversion','NSGI','DPnet_Saba_conversion','EPSG','3805','EPSG','1027');
+
+INSERT INTO "conversion" VALUES('NSGI','DPnet_St_Eustatius_conversion','Local map projection of St_Eustatius','','EPSG','9807','Transverse Mercator','EPSG','8801','Latitude of natural origin',0.0,'EPSG','9102','EPSG','8802','Longitude of natural origin',-63.0,'EPSG','9102','EPSG','8805','Scale factor at natural origin',0.9996,'EPSG','9201','EPSG','8806','False easting',1276.2485,'EPSG','9001','EPSG','8807','False northing',-2016.6406,'EPSG','9001',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+INSERT INTO "usage" VALUES('NSGI','DPnet_St_Eustatius_conversion_USAGE','conversion','NSGI','DPnet_St_Eustatius_conversion','EPSG','3805','EPSG','1027');
+
+--- Projected crs
+INSERT INTO "projected_crs" VALUES('NSGI','DPnet_Bonaire','Local coordinate system of Bonaire',NULL,'EPSG','4499','NSGI','DPnet_Bonaire_GEOGRAPHIC_2D','NSGI','DPnet_Bonaire_conversion',NULL,0);
+INSERT INTO "usage" VALUES('NSGI','DPnet_Bonaire_USAGE','projected_crs','NSGI','DPnet_Bonaire','EPSG','3805','EPSG','1027');
+
+INSERT INTO "projected_crs" VALUES('NSGI','DPnet_Saba','Local coordinate system of Saba',NULL,'EPSG','4499','NSGI','DPnet_Saba_GEOGRAPHIC_2D','NSGI','DPnet_Saba_conversion',NULL,0);
+INSERT INTO "usage" VALUES('NSGI','DPnet_Saba_USAGE','projected_crs','NSGI','DPnet_Saba','EPSG','3805','EPSG','1027');
+
+INSERT INTO "projected_crs" VALUES('NSGI','DPnet_St_Eustatius','Local coordinate system of St_Eustatius',NULL,'EPSG','4499','NSGI','DPnet_St_Eustatius_GEOGRAPHIC_2D','NSGI','DPnet_St_Eustatius_conversion',NULL,0);
+INSERT INTO "usage" VALUES('NSGI','DPnet_St_Eustatius_USAGE','projected_crs','NSGI','DPnet_St_Eustatius','EPSG','3805','EPSG','1027');
