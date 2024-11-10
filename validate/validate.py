778410eca0bd19d3638d8d65c4ace803dd348639
lhuisman <l.huisman@geopinie.nl>
2024-11-10 14:42:09 +0100

    Update validation to pyproj 3.7.0

--- validate/validate.py
+++ validate/validate.py
@@ -3,8 +3,11 @@ import sys
 
 print("PATH of the used PROJ: " + datadir.get_data_dir())
 
-rdnap2etrf = transformer.TransformerGroup("EPSG:7415", "EPSG:7931")
-etrf2rdnap = transformer.TransformerGroup("EPSG:7931", "EPSG:7415")
+rdnap2etrf2000 = transformer.TransformerGroup("EPSG:7415", "EPSG:7931")
+rd2etrf2000 = transformer.TransformerGroup("EPSG:28992", "EPSG:9067")
+etrf20002rdnap = transformer.TransformerGroup("EPSG:7931", "EPSG:7415")
+etrf20002rd = transformer.TransformerGroup("EPSG:9067", "EPSG:28992")
+
 
 result = (
     ",".join(
@@ -40,9 +43,14 @@ with open(sys.argv[1], "r") as Z001:
 
         if nap == "*":
             nap = float("inf")
+            h_calc = float("inf")
+            nap_calc = float("inf")
+            lat_calc, lon_calc = rd2etrf2000.transformers[0].transform(rdx, rdy)
+            rdx_calc, rdy_calc = etrf20002rd.transformers[0].transform(lat, lon)
 
-        lat_calc, lon_calc, h_calc = rdnap2etrf.transformers[0].transform(rdx, rdy, nap)
-        rdx_calc, rdy_calc, nap_calc = etrf2rdnap.transformers[0].transform(lat, lon, h)
+        else:
+            lat_calc, lon_calc, h_calc = rdnap2etrf2000.transformers[0].transform(rdx, rdy, nap)
+            rdx_calc, rdy_calc, nap_calc = etrf20002rdnap.transformers[0].transform(lat, lon, h)
 
         lat_diff, lon_diff, h_diff = (
             float(lat) - lat_calc,
